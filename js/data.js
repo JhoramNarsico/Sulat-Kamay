6efe8ba9e52b8d6d1518e68c8cf7265e357c34ed
JhoramNarsico <20220024450@my.xu.edu.ph>
2026-07-10 13:35:08 +0800

    updaed chocolates prices

--- js/data.js
+++ js/data.js
@@ -4,7 +4,7 @@ export const products = [
     { id: 3, name: "Curated Box 'Flowers'", price: 160.00, category: "box", img: "assets/giftbox-flowers.jpg" },
     { id: 4, name: "Personalized Letter", price: 120.00, category: "addon", img: "assets/letter-personalized.jpg", desc: "Choose a theme. Handwritten with wax seal." },
     { id: 5, name: "Flower Bouquet (Add-on)", price: 150.00, category: "addon", img: "assets/addon-bouquet.jpg", desc: "Mini dried flower bouquet." },
-    { id: 6, name: "Chocolates", price: 80.00, category: "addon", img: "assets/addon-choco.jpg", desc: "Sweet treats for your box." },
+    { id: 6, name: "Chocolates", price: 100.00, category: "addon", img: "assets/addon-choco.jpg", desc: "Sweet treats for your box." },
     { id: 7, name: "Polaroid-Inspired Photo", price: 5.00, category: "addon", img: "assets/polaroids.jpg", desc: "High quality print (per piece)." },
     { id: 8, name: "Aesthetic Stickers", price: 20.00, category: "addon", img: "assets/addon-stickers.jpg", desc: "Pack of vintage stickers." },
     { id: 9, name: "Hair Clips", price: 35.00, category: "addon", img: "assets/addon-clips.jpg", desc: "Cute aesthetic hair accessories." }
